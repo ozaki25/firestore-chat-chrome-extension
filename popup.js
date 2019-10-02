70cb113d62e8fc4af8fa33e6985829487ec2abf2
ozaki <fs65ugumori@yahoo.co.jp>
2019-10-03 08:45:06 +0900

    キー情報がセットされているかどうかでpopupの表示を切り替えるようにした

--- popup.js
+++ popup.js
@@ -1,6 +1,20 @@
 const checkbox = document.querySelector('#activate');
+const initialized = document.querySelector('.initialized');
+const uninitialized = document.querySelector('.uninitialized');
 
 function onLoad() {
+  chrome.storage.sync.get('apuKey', function(apiKey) {
+    chrome.storage.sync.get('projectId', function(projectId) {
+      if (apiKey && projectId) {
+        initialized.style.display = 'block';
+        uninitialized.style.display = 'none';
+      } else {
+        initialized.style.display = 'none';
+        uninitialized.style.display = 'block';
+      }
+    });
+  });
+
   chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
     const { url } = tabs[0];
     const { hostname } = new URL(url);
