db7ef1847ada7d48a04a626ce8c7adccf1f6d68c
ozaki <fs65ugumori@yahoo.co.jp>
2019-10-07 08:21:31 +0900

    新着がないときに過去分を出すようにした

--- inject.js
+++ inject.js
@@ -44,12 +44,12 @@ function main() {
 class Firestore {
   constructor({ firebase, apiKey, projectId }) {
     this.messages = [];
+    this.stock = [];
     this.prepend = false;
     this.firebase = firebase;
     if (!initialized) this.initFirebase({ apiKey, projectId });
     this.setDbRef();
     this.setListener();
-    this.render();
   }
 
   initFirebase({ apiKey, projectId }) {
@@ -62,13 +62,14 @@ class Firestore {
       .firestore()
       .collection('messages')
       .orderBy('timestamp', 'desc')
-      .limit(1);
+      .limit(10);
   }
 
   setListener() {
     this.unsubscribe = this.dbRef.onSnapshot(snapshot => {
-      // limit(1)なので1件しか来ない
-      snapshot.docs.forEach(doc => this.addMessage(doc.data()));
+      const messages = snapshot.docs.map(doc => doc.data());
+      this.addMessage(messages[0]);
+      this.addStock(messages);
     });
     const removeListener = e => {
       console.log('unsubscribe');
@@ -83,23 +84,39 @@ class Firestore {
     console.log('new message', { message }, 'old messagees', { messages: this.messages });
     const oldMessages = this.messages;
     this.messages = [...oldMessages, message];
+    // 空っぽだった時だけrender呼ぶ
+    // そうでなかったら一定時間おきに勝手に呼ばれるから明示的に呼ばない
     if (!oldMessages.length) this.render();
   }
 
+  addStock(messages) {
+    this.stock = messages;
+  }
+
   render() {
-    if (this.prepend || !this.messages.length) return;
+    if (this.prepend) return;
 
-    const message = this.messages[0];
-    this.prepend = true;
-    this.messages = [...this.messages.slice(1)];
-
-    removeMessage();
-    appendMessage(message);
+    if (this.messages.length) {
+      // 新着メッセージがあればそれを表示
+      const message = this.messages[0];
+      this.prepend = true;
+      this.messages = [...this.messages.slice(1)];
+      removeMessage();
+      appendMessage(message);
+    } else {
+      // 新着メッセージがなけばストッししておいた過去分を表示
+      if (!this.stock.length) return;
+      const message = this.stock[this.stock.length - 1];
+      this.prepend = true;
+      this.stock = [...this.stock.slice(0, -1)];
+      removeMessage();
+      appendMessage(message);
+    }
 
     setTimeout(() => {
       this.prepend = false;
       this.render();
-    }, 10000);
+    }, 8000);
   }
 }
 
