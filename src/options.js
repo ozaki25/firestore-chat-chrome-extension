3c987d621bc33e0bec81dc4410fac2413e758266
ozaki25 <fs65ugumori@yahoo.co.jp>
2019-10-10 18:44:37 +0900

    いろいろオプションで設定できるようにした

--- src/options.js
+++ src/options.js
@@ -1,27 +1,58 @@
+// vvariables
 const button = document.querySelector('#submit');
 const inputApiKey = document.querySelector('#apiKey');
 const inputProjectId = document.querySelector('#projectId');
+const inputDisplayTime = document.querySelector('#displayTime');
+const inputStockNumber = document.querySelector('#stockNumber');
+const inputInfiniteLoop = document.querySelector('#infiniteLoop');
 const clear = document.querySelector('#clear');
 
+// functions
 function onClickSubmit(e) {
   e.preventDefault();
   const apiKey = inputApiKey.value.trim();
   const projectId = inputProjectId.value.trim();
+  const displayTime = inputDisplayTime.value;
+  const stockNumber = inputStockNumber.value;
+  const infiniteLoop = inputInfiniteLoop.checked;
   if (!apiKey || !projectId) {
     alert('入力して下さい！！');
     return;
   }
-  chrome.storage.sync.set({ apiKey, projectId }, function() {
-    alert('保存しました！！');
-  });
+  chrome.storage.sync.set(
+    { apiKey, projectId, displayTime, stockNumber, infiniteLoop },
+    function() {
+      alert('保存しました！！');
+    }
+  );
 }
 
 function onClickClear(e) {
   e.preventDefault();
-  chrome.storage.sync.clear(function() {
-    alert('保存されたデータを削除しました');
-  });
+  const res = confirm('保存したデータを削除します\nよろしいですか？');
+  if (res) {
+    chrome.storage.sync.clear(function() {
+      alert('保存されたデータを削除しました');
+    });
+  }
 }
 
+function init() {
+  chrome.storage.sync.get(
+    ['apiKey', 'projectId', 'displayTime', 'stockNumber', 'infiniteLoop'],
+    function({ apiKey, projectId, displayTime, stockNumber, infiniteLoop }) {
+      inputApiKey.value = apiKey || '';
+      inputProjectId.value = projectId || '';
+      inputDisplayTime.value = displayTime || 8000;
+      inputStockNumber.value = stockNumber || 10;
+      inputInfiniteLoop.checked = infiniteLoop;
+    }
+  );
+}
+
+// listeners
 button.addEventListener('click', onClickSubmit);
 clear.addEventListener('click', onClickClear);
+
+// executes
+init();
